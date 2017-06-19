7560efa0877f21888e9b47dff8610e9654bce942
z <408869403@qq.com>
2017-06-20 00:18:44 +0800

    新增timestamps

--- admin/schemas/StoreCategory.js
+++ admin/schemas/StoreCategory.js
@@ -3,34 +3,9 @@ let mongoose = require('mongoose');
 // 商家分类
 let StoreCategorySchema = mongoose.Schema({
   // 分类名称
-  name: String,
-  meta: {
-    // 创建时间
-    create: {
-      type: Date,
-      default: new Date()
-    },
-    // 修改时间
-    update: {
-      type: Date,
-      default: new Date()
-    }
-  }
-});
-
-// 保存操作前做些处理
-StoreCategorySchema.pre('save', (next) => {
-  // 如果是新增，则同时设置create、update时间
-  if(this.isNew){
-    this.meta.create = this.meta.update = new Date();
-  }else{
-    console.log('---------------------');
-    console.log(this.meta);
-    // 如果是修改，则设置update
-    // this.meta.update = new Date();
-  }
-
-  next();
+  name: String
+}, {
+  timestamps: true
 });
 
 StoreCategorySchema.methods.speak = function () {
